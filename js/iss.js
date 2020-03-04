fbc40334a4a2240a61284370d1483493ed326ace
Brian Purnell <purnellbp@gmail.com>
2020-03-04 16:53:12 -0600

    live update

--- js/iss.js
+++ js/iss.js
@@ -1,2 +1,8 @@
+"use strict";
 // var issData = url('https://api.wheretheiss.at/v1/satellites/25544');
 
+mapboxgl.accessToken = mapBoxToken;
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9'
+});
