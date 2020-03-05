27e286c6b8150808f3eb1648edbf913c2fb56e0c
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-03-05 16:44:09 -0600

    working on function for tracker

--- js/iss.js
+++ js/iss.js
@@ -1,34 +1,50 @@
-"use strict";
-mapboxgl.accessToken = mapBoxToken;
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9'
-});
+$(document).ready(function () {
+    "use strict";
+//Access MapBox
+    mapboxgl.accessToken = mapBoxToken;
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9'
+    });
+
+    var url = 'https://api.wheretheiss.at/v1/satellites/25544';
 
 
-var issData = 'https://api.wheretheiss.at/v1/satellites/25544';
-var issDataGeoJSON = {
-    "type": "Feature",
-    "geometry": {
-        "type": "Point",
-        "coordinates": [fetch(issData).latitude, fetch(issData).longitude]
-    },
-    "properties": {
-        "name": ""
+    function refresh(url) {
+        var test = $.ajax(url);
+        var issDataGeoJSON = {};
+
+        test.done(function (jsonData) {
+            var lat = jsonData.latitude;
+            var lng = jsonData.longitude;
+
+            issDataGeoJSON = {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [lat, lng]
+                },
+                "properties": {
+                    "name": "test"
+                }
+            };
+        });
+        console.log(issDataGeoJSON);
+        return issDataGeoJSON;
     }
-};
 
-function getCoords() {
-    fetch(issData)
-        .then((response) => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error('no connection');
-            }
-        })
-        .then((json) => {
+    setInterval(refresh(url), 2000);
 
+    function getCoords() {
+        fetch(url)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('no connection');
+                }
+            })
+            .then((json) => {
                 var lat = json.latitude;
                 var lng = json.longitude;
                 var alt = json.altitude;
@@ -38,52 +54,50 @@ function getCoords() {
                 document.getElementById('lng').innerHTML = "Longitude: " + lng.toFixed(6);
                 document.getElementById('altitude').innerHTML = "Altitude: " + alt.toFixed(6);
                 document.getElementById('velocity').innerHTML = "Velocity: " + vel.toFixed(6);
-
-            }
-        );
-    setTimeout(getCoords, 5000);
-}
-
-getCoords();
-
-
-map.on('load', function () {
-    window.setInterval(function () {
-        map.getSource('drone').setData(issDataGeoJSON);
-    }, 2000);
-
-    map.addSource('drone', {type: 'geojson', data: issDataGeoJSON});
-    map.addLayer({
-        'id': 'drone',
-        'type': 'symbol',
-        'source': 'drone',
-        'layout': {
-            'icon-image': 'rocket-15'
-        }
-    });
-});
-
-
-var funFacts = [
-    {
-        Subject: "ISS",
-        Fact1: "The ISS is the largest manned object ever put into space!",
-        Fact2: "16 nations were involved in the construction of the ISS!",
-        Fact3: "The ISS travels at speeds upwards of 5 miles-per-second!",
-        Fact4: "The ISS is the single most expensive built in the history of mankind, estimated at over $120 billion dollars!"
-    },
-    {
-        Subject: "Space",
-        Fact1: "Space is Silent! Since there is no atmosphere in space, sound waves have no way of travelling!",
-        Fact2: "The first astronauts' footsteps on the moon will probably still be there 100 years from now!",
-        Fact3: "A little under 6 percent of our entire solar system's mass belongs to the sun!",
-        Fact4: "If 2 pieces of the same metal touch each other in space, they will bond together - a process known as 'cold welding'!",
-    },
-    {
-        Subject: "NASA",
-        Fact1: "ISS is an acronym for International Space Station!",
-        Fact2: "The first African-American woman in space was Dr. Mae Jemison in September of 1992!",
-        Fact3: "The first piloted mission in space was Apollo 7 in 1968!",
+            });
+        setTimeout(getCoords, 5000);
     }
-];
 
+    getCoords();
+
+    map.on('load', function () {
+        window.setInterval(function () {
+            map.getSource('drone').setData(refresh());
+        }, 2000);
+
+        map.addSource('drone', {type: 'geojson', data: refresh()});
+        map.addLayer({
+            'id': 'drone',
+            'type': 'symbol',
+            'source': 'drone',
+            'layout': {
+                'icon-image': 'rocket-15'
+            }
+        });
+    });
+
+
+    var funFacts = [
+        {
+            Subject: "ISS",
+            Fact1: "The ISS is the largest manned object ever put into space!",
+            Fact2: "16 nations were involved in the construction of the ISS!",
+            Fact3: "The ISS travels at speeds upwards of 5 miles-per-second!",
+            Fact4: "The ISS is the single most expensive built in the history of mankind, estimated at over $120 billion dollars!"
+        },
+        {
+            Subject: "Space",
+            Fact1: "Space is Silent! Since there is no atmosphere in space, sound waves have no way of travelling!",
+            Fact2: "The first astronauts' footsteps on the moon will probably still be there 100 years from now!",
+            Fact3: "A little under 6 percent of our entire solar system's mass belongs to the sun!",
+            Fact4: "If 2 pieces of the same metal touch each other in space, they will bond together - a process known as 'cold welding'!",
+        },
+        {
+            Subject: "NASA",
+            Fact1: "ISS is an acronym for International Space Station!",
+            Fact2: "The first African-American woman in space was Dr. Mae Jemison in September of 1992!",
+            Fact3: "The first piloted mission in space was Apollo 7 in 1968!",
+        }
+    ];
+
+});
