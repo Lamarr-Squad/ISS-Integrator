7225900b388723cfb164d0c28f3133e72035e1fc
Brian Purnell <purnellbp@gmail.com>
2020-03-04 23:20:55 -0600

    ran out of api requests, updated iss.js to use point movement

--- js/iss.js
+++ js/iss.js
@@ -18,6 +18,7 @@ function getCoords() {
             var alt = json.altitude;
             var vel = json.velocity;
 
+
             document.getElementById('lat').innerHTML = "Latitude: " + lat.toFixed(6);
             document.getElementById('lng').innerHTML = "Longitude: " + lng.toFixed(6);
             document.getElementById('altitude').innerHTML = "Altitude: " + alt.toFixed(6);
@@ -27,7 +28,6 @@ function getCoords() {
         );
     setTimeout(getCoords, 5000);
 }
-getCoords();
 
 var funFacts = [
     {
@@ -54,9 +54,133 @@ var funFacts = [
 
 
 
-mapboxgl.accessToken = mapBoxToken;
+// mapboxgl.accessToken = mapBoxToken;
+// var map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/mapbox/streets-v9'
+// });
+//
+//         // Assign to variable
+//         var jqXhr = $.ajax(issData);
+//
+// // Attach callback functions individually
+//
+//
+// function replayPos (){
+//         jqXhr.done(function (data, status, jqXhr) {
+//             var lat = data.latitude;
+//             var lng = data.longitude;
+//             var geojay =
+//                 {
+//                     "type": "Feature",
+//                     "geometry": {
+//                         "type": "Point",
+//                         "coordinates": [lng, lat]
+//                     },
+//                     "properties": {
+//                         "name": ""
+//                     }
+//                 };
+//             return geojay;
+//
+//             })
+//
+// } ;
+//
+//
+//             map.on('load', function () {
+//                 window.setInterval(function () {
+//                     map.getSource('drone').setData(url);
+//                     console.log("ping");
+//                 }, 2000);
+//             })
+//             map.addSource('drone', {type: 'geojson', data: url });
+//         map.addLayer({
+//             'id': 'drone',
+//             'type': 'symbol',
+//             'source': 'drone',
+//             'layout': {'icon-image': 'rocket-15'}
+//         })
+
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicHVybmVsbGJwIiwiYSI6ImNrNnV4d2dtbDBleHAzZnBudW14Z2VweGsifQ.TcGUbeIR8s-vagfNSK7x4w';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9'
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [0, 0],
+    zoom: 2
+});
+
+var radius = 20;
+
+function pointOnCircle() {
+
+    fetch(issData)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('no connection');
+            }
+        })
+        .then((json) => {
+
+                var lat = json.latitude;
+                var lng = json.longitude;
+                var alt = json.altitude;
+                var vel = json.velocity;
+
+
+                document.getElementById('lat').innerHTML = "Latitude: " + lat.toFixed(6);
+                document.getElementById('lng').innerHTML = "Longitude: " + lng.toFixed(6);
+                document.getElementById('altitude').innerHTML = "Altitude: " + alt.toFixed(6);
+                document.getElementById('velocity').innerHTML = "Velocity: " + vel.toFixed(6);
+
+
+            return {
+                'type': 'Point',
+                'coordinates': [lng, lat]
+            };
+
+
+            }
+
+
+
+
+        );
+    setTimeout(getCoords, 5000);
+
+}
+
+map.on('load', function() {
+// Add a source and layer displaying a point which will be animated in a circle.
+    map.addSource('point', {
+        'type': 'geojson',
+        'data': pointOnCircle()
+    });
+
+    map.addLayer({
+        'id': 'point',
+        'source': 'point',
+        'type': 'circle',
+        'paint': {
+            'circle-radius': 10,
+            'circle-color': '#007cbf'
+        }
+    });
+
+    function animateMarker(timestamp) {
+// Update the data to a new position based on the animation timestamp. The
+// divisor in the expression `timestamp / 1000` controls the animation speed.
+        map.getSource('point').setData(pointOnCircle());
+
+// Request the next frame of the animation.
+        requestAnimationFrame(animateMarker);
+    }
+
+// Start the animation.
+    animateMarker(0);
 });
 
