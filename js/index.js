73e05e29a01af42397ecbd7a5fd688e122b56cbe
Brian Purnell <purnellbp@gmail.com>
2020-03-06 08:52:48 -0600

    added header and moved facts div to top

--- js/index.js
+++ js/index.js
@@ -5,13 +5,14 @@ var pubnub = require("pubnub")({
 });
 
 function moveISS () {
-    $.get('http://api.open-notify.org/iss-now.json', function(error, response) {
+    $.get('https://api.wheretheiss.at/v1/satellites/25544', function(error, response) {
         var lat = response['iss_position']['latitude'];
         var lon = response['iss_position']['longitude'];
-
+        var alt = response['iss_position']['altitude'];
+        var vel = json.velocity;
         pubnub.publish({  //publishing the updated seat numbers through PubNub, in 'iss-pubnub' channel
             channel: "iss-pubnub",
-            message: {"latitude" : lat, "longitude" : lon}, //this is the message payload we are sending
+            message: {"latitude" : lat, "longitude" : lon, "altitude": alt}, //this is the message payload we are sending
             callback: function(m){console.log(m)}
         });
 
