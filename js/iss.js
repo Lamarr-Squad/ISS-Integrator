0a224fc0af2fbdbd42c1a0eaa7559da40712ade5
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-03-06 15:16:50 -0600

    cleaned up source code a little

--- js/iss.js
+++ js/iss.js
@@ -1,62 +1,107 @@
-"use strict";
-// var issData = url('https://api.wheretheiss.at/v1/satellites/25544');
-var issData = 'https://api.wheretheiss.at/v1/satellites/25544';
+$(document).ready(function () {
 
-function getCoords() {
-    fetch(issData)
-        .then((response) => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error('no connection');
-            }
+    // MapBox Access
+    L.mapbox.accessToken = 'pk.eyJ1IjoiYW5tb2wxNzcxIiwiYSI6ImNYZXN1N28ifQ.s-1Hjdb2UR9_6eeTPJGn3A'; //public key
+    var map = L.mapbox.map('map', 'mapbox.streets');
+
+    var marker = L.marker([-73, 40], {
+        icon: L.mapbox.marker.icon({
+            'marker-size': 'large',
+            'marker-symbol': 'rocket',
+            "marker-color": "#63b6e5"
         })
-        .then((json) => {
+    }).addTo(map);
+    marker.addTo(map);
 
-            var lat = json.latitude;
-            var lng = json.longitude;
-            var alt = json.altitude;
-            var vel = json.velocity;
+    //JSON api to get velocity and altitude
+    var issData = 'https://api.wheretheiss.at/v1/satellites/25544';
+    //Establish empty lat/lng for pubnub JS use
+    var lat, lon;
+    var pubnub = PUBNUB({subscribe_key: 'demo'});
+    // Add a new line to the map with no points.
+    var polyline = L.polyline([]).addTo(map);
+    // Keep a tally of how many points we've added to the map.
+    var pointsAdded = 0;
+    // Populate HTML element with our coordinates
+    var coordinates = document.getElementById('coordinates');
 
-            document.getElementById('lat').innerHTML = "Latitude: " + lat.toFixed(6);
-            document.getElementById('lng').innerHTML = "Longitude: " + lng.toFixed(6);
-            document.getElementById('altitude').innerHTML = "Altitude: " + alt.toFixed(6);
-            document.getElementById('velocity').innerHTML = "Velocity: " + vel.toFixed(6);
+    pubnub.subscribe({
+        channel: "iss-pubnub",
+        message: function (message) {
+            lat = message['latitude'];
+            lon = message['longitude'];
 
-            }
-        );
-    setTimeout(getCoords, 5000);
-}
-getCoords();
+            map.setView([lat, lon]);
+            marker.setLatLng([lat, lon], 15); //15 is the zoomlevel
 
-var funFacts = [
-    {
-        Subject: "ISS",
-        Fact1: "The ISS is the largest manned object ever put into space!",
-        Fact2: "16 nations were involved in the construction of the ISS!",
-        Fact3: "The ISS travels at speeds upwards of 5 miles-per-second!",
-        Fact4: "The ISS is the single most expensive built in the history of mankind, estimated at over $120 billion dollars!"
-    },
-    {
-        Subject: "Space",
-        Fact1: "Space is Silent! Since there is no atmosphere in space, sound waves have no way of travelling!",
-        Fact2: "The first astronauts' footsteps on the moon will probably still be there 100 years from now!",
-        Fact3: "A little under 6 percent of our entire solar system's mass belongs to the sun!",
-        Fact4: "If 2 pieces of the same metal touch each other in space, they will bond together - a process known as 'cold welding'!",
-    },
-    {
-        Subject: "NASA",
-        Fact1: "ISS is an acronym for International Space Station!",
-        Fact2: "The first African-American woman in space was Dr. Mae Jemison in September of 1992!",
-        Fact3: "The first piloted mission in space was Apollo 7 in 1968!",
+            polyline.addLatLng([lat, lon], pointsAdded);
+
+            coordinates.innerHTML = '<b>Latitude:</b> ' + lat + '<br /><b>Longitude:</b> ' + lon + '<br /><b>Velocity:</b> ' + vel + ' km/h' + '<br /><b>Altitude:</b> ' + alt + ' km' +
+                '<br /> <button id="liveButton" type="button" data-toggle="modal" data-target="#myModal">Live View</button>';
+        }
+    });
+
+    //Get velocity and altitude function
+    function getCoords() {
+        fetch(issData)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('no connection');
+                }
+            })
+            .then((json) => {
+                    var alt = json.altitude;
+                    var vel = json.velocity;
+                    window.alt = alt.toFixed(4);
+                    window.vel = vel.toFixed(4);
+                }
+            );
+        setTimeout(getCoords, 5000);
     }
-];
+    getCoords();
 
-
-
-mapboxgl.accessToken = mapBoxToken;
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9'
+    //Generate facts for top banner
+    function randomFact() {
+        var funFacts =
+            [
+                "The ISS is the largest manned object ever put into space!",
+                "16 nations were involved in the construction of the ISS!",
+                "The ISS travels at speeds upwards of 5 miles-per-second!",
+                "The ISS is the single most expensive built in the history of mankind, estimated at over $120 billion dollars!",
+                "Space is Silent! Since there is no atmosphere in space, sound waves have no way of travelling!",
+                "The first astronauts' footsteps on the moon will probably still be there 100 years from now!",
+                "A little under 6 percent of our entire solar system's mass belongs to the sun!",
+                "If 2 pieces of the same metal touch each other in space, they will bond together - a process known as 'cold welding'!",
+                "ISS is an acronym for International Space Station!",
+                "The first African-American woman in space was Dr. Mae Jemison in September of 1992!",
+                "The first piloted mission in space was Apollo 7 in 1968!"
+            ];
+        var x = funFacts.length;
+        var i = Math.floor((Math.random() * x) + 1);
+        return funFacts[i];
+    }
+    //Timer for facts
+    setTimeout(function () {
+        $('#theFacts').empty().append(randomFact());
+        console.log("ok go");
+    }, 5000);
 });
 
+//Code for mapbox popup - too much distortion as it follows marker
+// // create the popup
+// var popup = new mapboxgl.Popup({ offset: 25, anchor: 'bottom-left'})
+//     .setHTML("<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/EEIk7gwjgIM\" " +
+//         "frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>")
+//
+// // create DOM element for the marker
+// var el = document.createElement('div');
+// el.id = 'marker';
+//
+// // create the marker
+// new mapboxgl.Marker(el)
+//     .setLngLat(monument)
+//     .setPopup(popup) // sets a popup on this marker
+//     .addTo(map);
+//
